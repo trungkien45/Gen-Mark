4ca5c5fa8deb31da47315888bf696e1afece5f1c
kien <kien@kien.vn>
2023-12-26 09:37:13 +0700

    Gen dấu ký tự

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 32;
+            listBox1.Location = new Point(64, 114);
+            listBox1.Name = "listBox1";
+            listBox1.ScrollAlwaysVisible = true;
+            listBox1.Size = new Size(207, 324);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(64, 68);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(171, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(241, 68);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(328, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Globalization;
+using System.Text;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<string> convertKeyWord(string keyword)
+        {
+            //bool hasDiacritics = RemoveDiacritics(keyword) != keyword;
+
+            char[][] arr1 = new char[][] {
+               new char[] { 'a', 'á', 'à', 'ả', 'ã', 'ạ', 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ' , 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ'},
+               new char[] { 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ' },
+               new char[] { 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ' },
+               new char[] { 'd', 'đ' },
+               new char[] { 'e', 'é', 'è','ẻ','ẽ','ẹ', 'ê','ế','ề','ể','ễ','ệ' },
+               new char[] { 'ê', 'ế', 'ề','ể','ễ','ệ' },
+               new char[] { 'i', 'í', 'ì','ỉ','ĩ','ị' },
+               new char[] { 'o', 'ó', 'ò','ỏ','õ','ọ', 'ô','ô','ố','ồ','ổ','ỗ','ộ', 'ơ','ớ','ờ','ở','ỡ','ợ' },
+               new char[] { 'ô', 'ô', 'ố','ồ','ổ','ỗ','ộ' },
+               new char[] { 'ơ', 'ớ', 'ờ','ở','ỡ','ợ' },
+               new char[] { 'u', 'ú', 'ù','ủ','ũ','ụ', 'ư','ứ','ừ','ử','ữ','ự' },
+               new char[] { 'ư', 'ứ', 'ừ','ử','ữ','ự' },
+               new char[] { 'y', 'ý', 'ỳ','ỷ','ỹ','ỵ' }};
+            List<(int index, char value)> t = new List<(int, char)>();
+            for (int i = 0; i < keyword.Length; i++)
+            {
+                foreach (var item in arr1)
+                {
+                    if (keyword[i] == item[0])
+                    {
+                        t.Add((i, item[0]));
+                    }
+                }
+            }
+            var a = new List<string>();
+            //if (hasDiacritics)
+            BackTrackingMethod(0, keyword, arr1, a);
+            if (!a.Contains(keyword))
+                a.Insert(0, keyword);
+            return a;
+        }
+
+        private string RemoveDiacritics(string keyword)
+        {
+            var normalizedString = keyword.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        private void BackTrackingMethod(int z, string keyword, char[][] arr1, List<string> a)
+        {
+            if (z == keyword.Length)
+                return;
+            var x = arr1.Where(p => p[0] == keyword[z]).ToArray();
+            if (x.Length > 0)
+            {
+                var x1 = x[0];
+                for (int j = 0; j < x1.Length; j++)
+                {
+                    string k = keyword.Remove(z, 1).Insert(z, x1[j].ToString());
+                    if (!a.Contains(k))
+                        a.Add(k);
+                    BackTrackingMethod(z + 1, keyword.Remove(z, 1).Insert(z, x1[j].ToString()), arr1, a);
+                }
+            }
+            else
+            {
+                BackTrackingMethod(z + 1, keyword, arr1, a);
+            }
+        }
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var a = convertKeyWord(textBox1.Text.ToLower());
+            foreach (var item in a)
+            {
+                if (!string.IsNullOrEmpty(item))
+                    listBox1.Items.Add(item);
+            }
+
+        }
+    }
+}
